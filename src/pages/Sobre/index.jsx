a0a056fb36ffcffa93c3efb8efb7af61e06f8059
Lavínia Oliveira <68164917+lavinia-oliveira@users.noreply.github.com>
2024-02-16 12:44:36 -0300

    projetofinallavinia

---
+++ src/pages/Sobre/index.jsx
@@ -0,0 +1,39 @@
+import fluxoImg from '../../assets/fluxo.png'
+import fotoPerfil from '../../assets/foto-perfil.png'
+import styles from './sobre.module.css'
+
+const Sobre = () => {
+  return (
+    <>
+      <div className={styles.header}>
+        <h1>
+        Lavínia Oliveira
+        </h1>
+        <div>
+            <img src={fotoPerfil} alt="Foto de Perfil" />
+        </div>
+      </div>
+
+      <div className={styles.containerSobre} >
+          <h1 className={styles.nomeCompleto}>Lavínia Oliveira</h1>
+          <h2 className={styles.profissao}>Artista, Designer e Desenvolvedora Front-End</h2>
+          <p className={styles.sobreMim}>
+          Artista e designer, nascida em Salvador. 
+          </p>
+      </div>
+
+      <div className={styles.subtitulo}>
+        <img src={fluxoImg} />
+        <p>
+        Apaixonada por arte, música e tecnologia (como UX/UI e Front-End), busca constantemente por novas perspectivas e abordagens multidisciplinares.
+        </p>
+      </div>
+
+      <div className={styles.tituloSobre}>
+        Esses universos são evidenciados em suas criações que abraçam a inovação e transmitem uma energia singular, por meio de experiências que mesclam criatividade e os campos da comunicação.  
+      </div>
+    </>
+  )
+}
+
+export default Sobre;
